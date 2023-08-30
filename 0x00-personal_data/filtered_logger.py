ab289e56a15077f7e2953bb2c80a5465447e811e
TSOlami <tijani.saheed@yahoo.com>
2023-08-30 17:40:41 +0100

    add solutions with proper documentation

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,17 +1,29 @@
 #!/usr/bin/env python3
-
-from typing import List
+"""A module called filter_logger
+"""
 import re
 import logging
 from os import environ
 import mysql.connector
+from typing import List
 
+# List of fields considered as Personally Identifiable Information (PII)
 PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 
 
 def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
-    """"""
+    """Filter sensitive data fields in a log message.
+    
+    Args:
+        fields (List[str]): List of fields to obfuscate.
+        redaction (str): String to replace the obfuscated data with.
+        message (str): Log message containing data to be obfuscated.
+        separator (str): Character separating fields in the log message.
+    
+    Returns:
+        str: Log message with obfuscated data.
+    """
     for f in fields:
         message = re.sub(f'{f}=.*?{separator}',
                          f'{f}={redaction}{separator}', message)
@@ -19,6 +31,11 @@ def filter_datum(fields: List[str], redaction: str,
 
 
 def get_logger() -> logging.Logger:
+    """Get a configured logger instance.
+    
+    Returns:
+        logging.Logger: Logger instance for user data.
+    """
     logger = logging.getLogger("user_data")
     logger.setLevel(logging.INFO)
     logger.propagate = False
@@ -31,6 +48,11 @@ def get_logger() -> logging.Logger:
 
 
 def get_db() -> mysql.connector.connection.MySQLConnection:
+    """Get a database connection.
+    
+    Returns:
+        mysql.connector.connection.MySQLConnection: Database connection object.
+    """
     username = environ.get("PERSONAL_DATA_DB_USERNAME", "root")
     password = environ.get("PERSONAL_DATA_DB_PASSWORD", "")
     host = environ.get("PERSONAL_DATA_DB_HOST", "localhost")
@@ -44,6 +66,8 @@ def get_db() -> mysql.connector.connection.MySQLConnection:
 
 
 def main():
+    """Main function to retrieve and log filtered user data from the database.
+    """
     db = get_db()
     cursor = db.cursor()
     cursor.execute("SELECT * FROM users;")
@@ -60,17 +84,21 @@ def main():
 
 
 class RedactingFormatter(logging.Formatter):
-    """Redacting Formatter class
+    """Redacting Formatter class for logging.
     """
     REDACTION = "***"
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
     def __init__(self, fields: List[str]):
+        """Initialize the object
+        """
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
+        """Method for formatting
+        """
         record.msg = filter_datum(self.fields, self.REDACTION,
                                   record.getMessage(), self.SEPARATOR)
         return super(RedactingFormatter, self).format(record)
