c0d33b3ffa12b5c93bae133b094209638bf1b16e
TSOlami <tijani.saheed@yahoo.com>
2023-09-05 12:49:07 +0100

    fix pep 8

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -7,21 +7,19 @@ from typing import List, TypeVar
 
 
 class Auth():
-	"""Returns authentication
-	"""
-	def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-		"""Returns False
-		"""
-		return False
+    """Returns authentication
+    """
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """Returns bool
+        """
+        return False
 
+    def authorization_header(self, request=None) -> str:
+        """Returns str
+        """
+        return None
 
-	def authorization_header(self, request=None) -> str:
-		"""Returns None
-		"""
-		return None
-
-
-	def current_user(self, request=None) -> TypeVar('User'):
-		"""Returns None
-		"""
-		return None
+    def current_user(self, request=None) -> TypeVar('User'):
+        """Returns str
+        """
+        return None
